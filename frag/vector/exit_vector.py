4e08a104b5e4be7d98434ebdf72c2cb5912ce3ee
abradle <anthony.richard.bradley@gmail.com>
2017-09-28 10:40:41 +0100

    Updates to python 3

--- frag/vector/exit_vector.py
+++ frag/vector/exit_vector.py
@@ -4,7 +4,7 @@ from frag.utils.vector_utils import get_max_ev_smi
 
 if __name__ == "__main__":
     for item in [x.strip().split(",") for x in open("/Users/abradley/fragalysis/frag/tests/data/enamine.csv").readlines()]:
-        print item
+        print(item)
         smiles = decorate_smi(item[1])
         code = item[0]
         for smi in smiles:
